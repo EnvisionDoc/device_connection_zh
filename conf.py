b6b2a1b62910f25c4bcb16cd2b0431fd5aee65a2
EnvisionIoT <37394998+EnvisionIot@users.noreply.github.com>
2019-10-22 14:47:00 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -120,7 +120,7 @@ html_theme = 'sphinx_enos_theme'
 #
 html_theme_options = {
     'copyright_zh': '© 2019 Envision Digital. All Rights Reserved.',
-    'docs_url_zh': '/zh_CN/documentation-2/',
+    'docs_url_zh': '/zh/documentation-2/',
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
